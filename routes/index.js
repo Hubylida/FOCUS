35a2f13f16dec25528b7ec0cd3d27c2868fb9ca3
Hubylida <hubylidayuan@outlook.com>
2017-10-03 18:31:23 +0800

    new

--- routes/index.js
+++ routes/index.js
@@ -26,8 +26,14 @@ router.get('/about', function(req, res, next) {
   res.render('about');
 });
 
-router.get('/members_1',function(req,res,next){
-  res.render('member',{"name":data[0].name,"title":data[0].title,"head_img":data[0].head_img,"intro":data[0].intro,"phone":data[0].phone,"Email":data[0].Email});
-})
+// router.get('/members_1',function(req,res,next){
+//   res.render('member',{"name":data[0].name,"title":data[0].title,"head_img":data[0].head_img,"intro":data[0].intro,"phone":data[0].phone,"Email":data[0].Email});
+// })
+
+for(let i = 0; i < 54; i++){
+  router.get('/members_' + i,function(req,res,next){
+    res.render('member',{"name":data[0].name,"title":data[0].title,"head_img":data[0].head_img,"intro":data[0].intro,"phone":data[0].phone,"Email":data[0].Email});
+  });
+}
 
 module.exports = router;
