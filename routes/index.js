4fc8682187054314ccef74e32deddccd08923ef4
Hubylida <hubylidayuan@outlook.com>
2017-10-03 14:24:43 +0800

    new

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var path = require('path');
+var data_str = fs.readFileSync('members.json',{encoding:'UTF-8'}),
+    data = JSON.parse(data_str);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -21,4 +25,9 @@ router.get('/events', function(req, res, next) {
 router.get('/about', function(req, res, next) {
   res.render('about');
 });
+
+router.get('/members_1',function(req,res,next){
+  res.render('member',{"name":data[0].name,"title":data[0].title,"head_img":data[0].head_img,"intro":data[0].intro,"phone":data[0].phone,"Email":data[0].Email});
+})
+
 module.exports = router;
