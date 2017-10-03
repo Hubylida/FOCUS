4b1b69d184b490037115870ee044166ff46a9e2b
Hubylida <hubylidayuan@outlook.com>
2017-10-03 22:38:25 +0800

    new

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,33 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path = require('path');
-var data_str = fs.readFileSync('members.json',{encoding:'UTF-8'}),
-    data = JSON.parse(data_str);
+var data_str = fs.readFileSync('members.json', {
+    encoding: 'UTF-8'
+  }),
+  data = JSON.parse(data_str);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 
-router.get('/members', function(req, res, next) {
+router.get('/members', function (req, res, next) {
   res.render('members');
 });
 
-router.get('/research', function(req, res, next) {
+router.get('/research', function (req, res, next) {
   res.render('research');
 });
 
-router.get('/events', function(req, res, next) {
+router.get('/events', function (req, res, next) {
   res.render('events');
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/join', function (req, res, next) {
+  res.render('join');
+})
+
+router.get('/about', function (req, res, next) {
   res.render('about');
 });
 
@@ -30,9 +36,16 @@ router.get('/about', function(req, res, next) {
 //   res.render('member',{"name":data[0].name,"title":data[0].title,"head_img":data[0].head_img,"intro":data[0].intro,"phone":data[0].phone,"Email":data[0].Email});
 // })
 
-for(let i = 0; i < 54; i++){
-  router.get('/members_' + i,function(req,res,next){
-    res.render('member',{"name":data[0].name,"title":data[0].title,"head_img":data[0].head_img,"intro":data[0].intro,"phone":data[0].phone,"Email":data[0].Email});
+for (let i = 0; i < 54; i++) {
+  router.get('/members_' + i, function (req, res, next) {
+    res.render('member', {
+      "name": data[0].name,
+      "title": data[0].title,
+      "head_img": data[0].head_img,
+      "intro": data[0].intro,
+      "phone": data[0].phone,
+      "Email": data[0].Email
+    });
   });
 }
 
