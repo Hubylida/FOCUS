511f5857b09503046f186c28d94eea9ca47d2cea
Hubylida <hubylidayuan@outlook.com>
2017-10-06 11:49:37 +0800

    new

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -42,7 +42,7 @@ module.exports = {
       host : '101.236.29.56',
       ref  : 'origin/master',
       repo : 'git@github.com:Hubylida/FOCUS.git',
-      path : '/var/root/www/development/FOCUS',
+      path : '/Documents/Github/FOCUS',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
       env  : {
         NODE_ENV: 'dev'
